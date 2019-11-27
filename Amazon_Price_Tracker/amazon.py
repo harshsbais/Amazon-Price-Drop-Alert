533c5836655ce9fee4d2e241805a7383785e7209
Harsh Singh Bais <43093686+harshsbais@users.noreply.github.com>
2019-11-28 01:35:06 +0530

    Update amazon.py

--- Amazon_Price_Tracker/amazon.py
+++ Amazon_Price_Tracker/amazon.py
@@ -91,8 +91,6 @@ def check_price():
 
 
 def send_mail(name, usr_email):
-    # print("*************************\n")
-    # print(name+" "+usr_email)
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
@@ -114,29 +112,23 @@ root.title("Amazon Price Drop Alert")
 
 label_1 = Label(root, text="Please Enter the URL:")
 entry_1 = Entry(root)
-#button_1 = Button(root, text="Click to enter URL", command=enter_url)
 
 label_2 = Label(root, text="Please Enter the Price:")
 entry_2 = Entry(root)
-#button_2 = Button(root, text="Click to enter price", command=enter_price)
 
 label_3 = Label(root, text="Please Enter your email address:")
 entry_3 = Entry(root)
-#button_3 = Button(root, text="Click to enter email", command=enter_email)
 
 button_4 = Button(root, text="Click to check", command=check_price)
 
 label_1.grid(row=0, column=0)
 entry_1.grid(row=0, column=1)
-#button_1.grid(row=0, column=2)
 
 label_2.grid(row=1, column=0)
 entry_2.grid(row=1, column=1)
-#button_2.grid(row=1, column=2)
 
 label_3.grid(row=2, column=0)
 entry_3.grid(row=2, column=1)
-#button_3.grid(row=2, column=2)
 
 button_4.grid(row=3, column=1)
 
